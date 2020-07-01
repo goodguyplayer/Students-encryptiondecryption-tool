01f9ab3559a2bc0f68acad1b716ba2b6d5202f0d
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-07-01 19:32:20 -0300

    CaesarCipher - Added description method

--- src/com/company/methods/caesarcipher/CaesarCipher.java
+++ src/com/company/methods/caesarcipher/CaesarCipher.java
@@ -1,5 +1,18 @@
 package com.company.methods.caesarcipher;
 
 public class CaesarCipher {
-    
+
+    private void description(){
+        System.out.println("Caesar Cipher. \n" +
+                "A favourite classic. \n" +
+                "\n" +
+                "Encryption.: \n" +
+                " - Takes a string and the decimal representation to them. \n" +
+                " - For every letter, walk forward or back \"x\" positions. \n" +
+                " - Return result. If overflown (Like, say, 26), just repeat back to the first letter and add the rest. \n" +
+                "\n" +
+                "Decryption.: \n" +
+                " - Almost like the encryption, just going back (or forward) x positions");
+    }
+
 }
