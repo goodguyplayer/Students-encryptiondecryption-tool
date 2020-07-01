70321c3c9683ac3106a023d8003e63ae7cae86e6
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-07-01 10:43:40 -0300

    ggpsfuncrypto - Encrypt updated with randomNumbers method

--- src/com/company/methods/ggpsfuncrypto/Encrypt.java
+++ src/com/company/methods/ggpsfuncrypto/Encrypt.java
@@ -2,11 +2,12 @@ package com.company.methods.ggpsfuncrypto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 class Encrypt {
     public String encrypt(String input){
         // String for outputting the result
-        String output = new String();
+        String output = "";
         int size = 0;
 
         //Arrays for the numbers
@@ -14,6 +15,16 @@ class Encrypt {
         ArrayList<Integer> middle = new ArrayList<Integer>();
         ArrayList<Integer> back = new ArrayList<Integer>();
 
+        return output;
+    }
+
+    // Random number generator
+    private String randomNumbers(int size){
+        Random random = new Random();
+        String output = "";
+        for (int i = 0; i < size ; i++) {
+            output += random.nextInt((1000-100) + 1) + 100;
+        }
         return output;
     }
 
