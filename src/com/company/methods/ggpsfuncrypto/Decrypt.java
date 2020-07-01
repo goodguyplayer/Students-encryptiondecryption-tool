9c30fa5c10ce4a88c07eba61ae02c46ec3a77b9d
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-07-01 12:48:45 -0300

    GGPsFunCrypto - Decrypt class - Added decrypt method

--- src/com/company/methods/ggpsfuncrypto/Decrypt.java
+++ src/com/company/methods/ggpsfuncrypto/Decrypt.java
@@ -1,5 +1,28 @@
 package com.company.methods.ggpsfuncrypto;
+import java.util.ArrayList;
+import java.util.List;
 
 class Decrypt {
-    
+
+    // Main method for decryption
+    public String decrypt(String input){
+        int size = input.length();
+        int counter = 0;
+        String helper = "";
+        String output = "";
+
+        // Store each "character" in the array
+        for (int i = 0; i < size/9; i++) {
+            helper += input.charAt(i);
+            counter += 1;
+
+            // Once we reach the first 3 characters, convert it into String and add it.
+            if (counter == 3){
+                counter = 0;
+                output += Integer.toString(Integer.parseInt(helper,8));
+            }
+        }
+        
+        return output;
+    }
 }
