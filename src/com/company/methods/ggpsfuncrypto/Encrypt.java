ea10161030dff0d707bac13d2e4af797c79c6036
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-07-01 10:34:23 -0300

    ggpsfuncrypto - Encrypt class updated
    Added basics for the encryption method

--- src/com/company/methods/ggpsfuncrypto/Encrypt.java
+++ src/com/company/methods/ggpsfuncrypto/Encrypt.java
@@ -1,5 +1,20 @@
 package com.company.methods.ggpsfuncrypto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Encrypt {
+    public String encrypt(String input){
+        // String for outputting the result
+        String output = new String();
+        int size = 0;
+
+        //Arrays for the numbers
+        ArrayList<Integer> front = new ArrayList<Integer>();
+        ArrayList<Integer> middle = new ArrayList<Integer>();
+        ArrayList<Integer> back = new ArrayList<Integer>();
+
+        return output;
+    }
 
 }
