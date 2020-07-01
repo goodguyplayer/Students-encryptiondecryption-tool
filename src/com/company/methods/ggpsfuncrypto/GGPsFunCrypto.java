8bbae99fbc7e01632e2983d3792fddef6a061c56
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-07-01 10:59:04 -0300

    GGPsFunCrypto - Description updated

--- src/com/company/methods/ggpsfuncrypto/GGPsFunCrypto.java
+++ src/com/company/methods/ggpsfuncrypto/GGPsFunCrypto.java
@@ -9,9 +9,9 @@ public class GGPsFunCrypto {
         System.out.println(
                 "GGP's Fun crypto \n" +
                         "Encryption.: \n" +
-                        " - It first takes a string, calculates the size of it. Then, it'll convert the string in a list of oct.  \n" +
-                        " - For said size, it'll start 2 lists (\"Back\" and \"Front\") and fill it with random numbers (from 100 to 999). \n" +
-                        " - After that, it'll combine \"Front\" + main list (oct) + \"Back\" into one large string and output it. \n" +
+                        " - It first takes a string, calculates the size of it. Then, it'll convert the string in a string of oct.  \n" +
+                        " - For said size, it'll start 2 string (\"Back\" and \"Front\") and fill it with random numbers (from 100 to 999). \n" +
+                        " - After that, it'll combine \"Front\" + main (oct) + \"Back\" into one large string and output it. \n" +
                         "\n" +
                         "Decryption.: \n" +
                         " - It first takes the string, calculates the size of it. Then split it into 3 strings, taking the middle. \n" +
